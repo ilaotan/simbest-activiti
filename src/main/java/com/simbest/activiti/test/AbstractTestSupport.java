dd60e5452ae32b514870b0b87e3e6ef95d286ca2
kyne0116 <kyne.li@hotmail.com>
2016-07-01 16:59:16 +0800

    统一升级版本

---
+++ src/main/java/com/simbest/activiti/test/AbstractTestSupport.java
@@ -0,0 +1,95 @@
+package com.simbest.activiti.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.activiti.engine.FormService;
+import org.activiti.engine.HistoryService;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ManagementService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.history.HistoricDetail;
+import org.activiti.engine.history.HistoricFormProperty;
+import org.activiti.engine.history.HistoricVariableUpdate;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.simbest.cores.test.AbstractComponentTester;
+
+public class AbstractTestSupport extends AbstractComponentTester{
+    @Autowired
+    protected ProcessEngine processEngine;
+    
+    @Autowired
+    protected RepositoryService repositoryService;
+   
+    @Autowired
+    protected RuntimeService runtimeService;
+    
+    @Autowired
+    protected TaskService taskService;
+    
+    @Autowired
+    protected HistoryService historyService;
+    
+    @Autowired
+    protected IdentityService identityService;
+    
+    @Autowired
+    protected ManagementService managementService;
+    
+    @Autowired
+    protected FormService formService;
+
+    /**
+     * 开始测试
+     */
+    @BeforeClass
+    public static void setUpForClass() throws Exception {
+        System.out.println("++++++++ 开始测试 ++++++++");
+    }
+
+    /**
+     * 结束测试
+     */
+    @AfterClass
+    public static void testOverForClass() throws Exception {
+        System.out.println("-------- 结束测试 --------");
+    }
+
+    /**
+     * 初始化变量
+     */
+    @Before
+    public void setUp() throws Exception {
+
+    }
+    
+    /**
+     * 读取历史变量并封装到Map中
+     */
+    protected Map<String, Object> packageVariables(ProcessInstance processInstance) {
+        Map<String, Object> historyVariables = new HashMap<String, Object>();
+        List<HistoricDetail> list = historyService.createHistoricDetailQuery().processInstanceId(processInstance.getId()).list();
+        for (HistoricDetail historicDetail : list) {
+            if (historicDetail instanceof HistoricFormProperty) {
+                // 表单中的字段
+                HistoricFormProperty field = (HistoricFormProperty) historicDetail;
+                historyVariables.put(field.getPropertyId(), field.getPropertyValue());
+                System.out.println("form field: taskId=" + field.getTaskId() + ", " + field.getPropertyId() + " = " + field.getPropertyValue());
+            } else if (historicDetail instanceof HistoricVariableUpdate) {
+                HistoricVariableUpdate variable = (HistoricVariableUpdate) historicDetail;
+                historyVariables.put(variable.getVariableName(), variable.getValue());
+                System.out.println("variable: " + variable.getVariableName() + " = " + variable.getValue());
+            }
+        }
+        return historyVariables;
+    }
+}
